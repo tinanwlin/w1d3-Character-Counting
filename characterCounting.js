bbaf2c84d77f4cf0f148a51b51fd35e65999a108
tinanwlin <tinanwlin@gmail.com>
2018-02-14 22:31:56 +0000

    Complete the countLetters function.

---
+++ characterCounting.js
@@ -0,0 +1,18 @@
+function countLetters(str) {
+
+    var letterCount = {};
+
+    for (var i = 0; i < str.length; i++) {
+        var eachLetter = str[i];
+        if (eachLetter !== " ") {
+            if (letterCount[eachLetter] === undefined) {
+                letterCount[eachLetter] = 0;
+            }
+            letterCount[eachLetter] += 1;
+        }
+    }
+    return letterCount;
+}
+
+console.log(countLetters("ab b"));
+
